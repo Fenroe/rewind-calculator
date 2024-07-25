fc65517b8b2037b44fd37edd8c5d3fbaac838846
fenroe <scrambles9494@gmail.com>
2024-07-25 12:45:19 +0300

    Wrote functions for basic maths operations and tested them

---
+++ main.js
@@ -0,0 +1,11 @@
+// basic calculator operations
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+const subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+
+// log tests
+console.log(add(12, 15));
+console.log(subtract(40, 15));
+console.log(multiply(7, 4));
+console.log(divide(100, 10));
