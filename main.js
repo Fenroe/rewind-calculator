1899710be50703ed6d09bcd8c1a080bbc72e71ca
fenroe <scrambles9494@gmail.com>
2024-07-25 16:11:00 +0300

    Calculator now works

--- main.js
+++ main.js
@@ -7,26 +7,126 @@ const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
 const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
 
 // variables
-let firstNumber = 0;
+const operators = ["+", "-", "/", "*"];
+let firstNumber = null;
 let operator = "";
-let secondNumber = 0;
+let secondNumber = null;
+let displayValue = "0";
+
+// DOM
+const displayText = document.getElementById("display-text");
 
 // operate function
 const operate = (firstNumber, secondNumber, operator) => {
   switch (operator) {
-    case "add":
+    case "+":
       return add(firstNumber, secondNumber);
-    case "subtract":
+    case "-":
       return subtract(firstNumber, secondNumber);
-    case "multiply":
+    case "*":
       return multiply(firstNumber, secondNumber);
-    case "divide":
-      return divide(firstNumber, secondNumber);
+    case "/":
+      return +divide(firstNumber, secondNumber).toFixed(8);
   }
 };
 
-// log tests
-console.log(add(12, 15));
-console.log(subtract(40, 15));
-console.log(multiply(7, 4));
-console.log(divide(100, 10));
+const updateDisplayValue = (input) => {
+  if (input === "+/-") {
+    if (displayValue.includes("-")) {
+      displayValue = displayValue.slice(1);
+    } else {
+      displayValue = `-${displayValue}`;
+    }
+  } else if (input === "%") {
+    displayValue = `${+displayValue / 100}`;
+  } else if (input === "." && displayValue.length < 8) {
+    if (!displayValue.includes(".")) {
+      displayValue = `${displayValue}${input}`;
+    }
+  } else if (displayValue === "0") {
+    displayValue = input;
+  } else {
+    if (displayValue.length < 9) {
+      displayValue = `${displayValue}${input}`;
+    }
+  }
+  if (displayValue.length > 10) {
+    resetCalculator();
+    displayText.textContent = "too long";
+  } else {
+    displayText.textContent = displayValue;
+    if (operator === "") {
+      firstNumber = +displayValue;
+    } else {
+      secondNumber = +displayValue;
+    }
+  }
+};
+
+const storeDisplayValue = () => {
+  const convertedDisplayValue = +displayValue;
+  firstNumber = convertedDisplayValue;
+  displayValue = "0";
+};
+
+const resetCalculator = () => {
+  firstNumber = null;
+  operator = "";
+  secondNumber = null;
+  displayValue = "0";
+  displayText.textContent = displayValue;
+};
+
+const handleButtonInput = (buttonText) => {
+  if (buttonText === "AC") {
+    return resetCalculator();
+  } else if (operators.includes(buttonText)) {
+    if (operator === "" || secondNumber === null) {
+      operator = buttonText;
+      displayValue = "0";
+    } else {
+      const result = operate(firstNumber, secondNumber, operator);
+      firstNumber = result;
+      operator = "";
+      secondNumber = null;
+      displayValue = `${result}`;
+      if (displayValue.length > 10) {
+        resetCalculator();
+        displayText.textContent = "too long";
+      } else {
+        displayText.textContent = displayValue;
+        displayValue = "0";
+      }
+    }
+  } else if (buttonText === "=") {
+    if (firstNumber !== null && secondNumber !== null && operator !== "") {
+      const result = operate(firstNumber, secondNumber, operator);
+      firstNumber = result;
+      operator = "";
+      secondNumber = null;
+      displayValue = `${result}`;
+      if (displayValue.length > 10) {
+        resetCalculator();
+        displayText.textContent = "too long";
+      } else {
+        displayText.textContent = displayValue;
+        displayValue = "0";
+      }
+    } else if (firstNumber !== null) {
+      const savedFirstNumnber = firstNumber;
+      resetCalculator();
+      updateDisplayValue(`${savedFirstNumnber}`);
+    } else {
+      resetCalculator();
+    }
+  } else {
+    updateDisplayValue(buttonText);
+  }
+};
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    handleButtonInput(button.textContent);
+  });
+});
